d04a899e380f568dc735ad6bb2d0ad39cb90b5a9
Steven <luwenxian6@gmail.com>
2020-09-26 21:17:32 +0800

    STM32CubeMX生成nucleo_f302r8的工程。

---
+++ rtthread_nucleo_f302r8/MDK-ARM/RTE/_rtthread_nucleo_f302r8/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'rtthread_nucleo_f302r8' 
+ * Target:  'rtthread_nucleo_f302r8' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f3xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
